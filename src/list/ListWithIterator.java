ea2e6e1935c4c02c3427f92d267179f2127b7c7c
Minisiia <ar_tik@yahoo.com>
2022-12-13 22:01:30 +0200

    Java Advanced 1. Iterator & Comparator

---
+++ src/list/ListWithIterator.java
@@ -0,0 +1,20 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListWithIterator {
+    public static void main(String[] args) {
+        List<Integer> integerList=new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            integerList.add(i + 1);
+        }
+
+        Iterator <Integer> integerIterator = integerList.iterator();
+        while ((integerIterator.hasNext())){
+            Integer temp = integerIterator.next();
+            System.out.println(temp);
+        }
+    }
+}
